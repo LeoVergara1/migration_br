6ca4a255832b198f384c6f7eb46c0b7d0fe0b7c1
Brandon Vergara <brandon.vergara@kueski.com>
2023-05-05 00:18:22 -0600

    [master] finish process

--- migration.rb
+++ migration.rb
@@ -16,13 +16,14 @@ begin
   puts "Database name: #{db_name}"
   puts "Password: #{pswd}"
 
-  rs = con.exec "SELECT * FROM users_filter LIMIT 10"
+  rs = con.exec "select count(*) from users_filter uf where user_id  between 0 and 300000000"
   string_list = "("
   CSV.open("migration.csv", "w") do |csv|
     csv << helper.headers
-    map = rs.each do |row|
+    map = rs.each_with_index do |row, index|
       csv << helper.map_row(row)
       string_list = string_list + "'#{row['user_id']}',"
+      p index if (index%100000 === 0)
     end
   end
   string_list = string_list.chop
